82d82feb1c8aeea9d77b9cd03f48fcc4e3188e38
sgrpanchal31 <sgrpanchal31@gmail.com>
2018-06-30 21:51:35 +0530

    changed the detection.py code

--- detection.py
+++ detection.py
@@ -10,22 +10,22 @@ def main():
     ap.add_argument("-i", "--image", required=True, help="path to the input image")
     args = ap.parse_args()
 
-
+    #Make the table detector class and predict the score
     obj = NutritionTableDetector()
     image = Image.open(args.image)
     boxes, scores, classes, num  = obj.get_classification(image)
     width, height = image.size
-    print(scores)
 
+    #Select the bounding box with most confident output
     ymin = boxes[0][0][0]*height
     xmin = boxes[0][0][1]*width
     ymax = boxes[0][0][2]*height
     xmax = boxes[0][0][3]*width
 
     # print(xmin, ymin, xmax, ymax, scores[0][0])
-
     coords = (xmin, ymin, xmax, ymax)
 
+    #Crop the image with the given bounding box
     cropped_image = crop_img(image, coords, "./")
     cropped_image.show()
 
