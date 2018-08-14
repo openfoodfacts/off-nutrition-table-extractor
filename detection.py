ba5010fd21312de1f7c7bcd2424e01ebaf28a96f
odtvince <vince@data.gouv.fr>
2018-08-14 21:51:19 +0200

    better fuzzy matching of the nutrients

--- detection.py
+++ detection.py
@@ -8,10 +8,10 @@ from crop import crop
 from text_detection import text_detection
 from process import *
 from regex import *
-from nutrient_list import make_list
+from nutrient_list import *
 from spacial_map import *
 
-def detect(img_path):
+def detect(img_path, debug):
     """
     @param img_path: Pathto the image for which labels to be extracted
     """
@@ -27,8 +27,9 @@ def detect(img_path):
     width = image.shape[1]
     height = image.shape[0]
 
-    time_taken = time.time() - start_time
-    print("Time taken to detect the table: %.5fs" % time_taken)
+    if debug:
+        time_taken = time.time() - start_time
+        print("Time taken to detect the table: %.5fs" % time_taken)
 
     #Select the bounding box with most confident output
     ymin = boxes[0][0][0]*height
@@ -43,23 +44,36 @@ def detect(img_path):
     cropped_image = crop(image, coords, "./data/result/output.jpg", 0, True)
 
     #Apply several filters to the image for better results in OCR
-    cropped_image = preprocess_for_ocr(cropped_image)
-    
+    cropped_image = preprocess_for_ocr(cropped_image, 3)
+
+    if debug:
+        cv2.imwrite('./data/result/output-opt.png', cropped_image)
+
     #detecting the text
     text_blob_list = text_detection(cropped_image)
-    time_taken = time.time() - start_time
-    print("Time Taken to detect bounding boxes for text: %.5fs" % time_taken)
-    # print(text_blob_list)
+
+    if debug:
+        time_taken = time.time() - start_time
+        print("Time Taken to detect bounding boxes for text: %.5fs" % time_taken)
+        # print(text_blob_list)
 
     text_location_list = []   #store all the metadata of every text box
     nutrient_dict = {}        # Dictionary to store nutrient labels and their values
 
+    counter=0
     #Apply OCR to to blobs and save data in organized dict
     for blob_cord in text_blob_list:
-        word_image = crop(cropped_image, blob_cord, "./", 0.005, False)
+        if debug:
+            counter+=1
+            word_image = crop(cropped_image, blob_cord, "./data/result/{}.jpg".format(counter), 0.005, True)
+        else:
+            word_image = crop(cropped_image, blob_cord, "./", 0.005, False)
         word_image = preprocess_for_ocr(word_image)
-        text = ocr(word_image)
- 
+        text = ocr(word_image,1,7)
+
+        if debug:
+            print(text)
+
         if text:
             center_x = (blob_cord[0]+blob_cord[2])/2
             center_y = (blob_cord[1]+blob_cord[3])/2
@@ -73,7 +87,7 @@ def detect(img_path):
             }
             text_location_list.append(new_location)
 
-    #Spacial algorithm that maps all boxes according to their location and append the string 
+    #Spatial algorithm that maps all boxes according to their location and append the string
     for text_dict in text_location_list:
         if(text_dict['string_type']==2):
             for text_dict_test in text_location_list:
@@ -81,19 +95,22 @@ def detect(img_path):
                     text_dict['text'] = text_dict['text'].__add__(' '+text_dict_test['text'])
                     text_dict['string_type'] = 0
 
+    fuzdict=make_fuzdict('data/nutrients.txt')
+
     #Add the nutritional label and its value to the nutrient_dict
     for text_dict in text_location_list:
-               
+
         if(text_dict['string_type']==0):
-            # print(text_dict['text']) 
             text = clean_string(text_dict['text'])
 
-            if check_for_label(text, make_list('data/big.txt')):
-                label_name, label_value = get_label_from_string(text)
+#             if check_for_label(text, make_list('data/nutrients.txt')):
+            if fuz_check_for_label(text, fuzdict, debug):
+                label_name, label_value = get_fuz_label_from_string(text, fuzdict, debug)
                 nutrient_dict[label_name] = separate_unit(label_value)
-    
-    time_taken = time.time() - start_time
-    print("Total Time Taken: %.5fs" % time_taken)
+
+    if debug:
+        time_taken = time.time() - start_time
+        print("Total Time Taken: %.5fs" % time_taken)
 
     return nutrient_dict
 
@@ -102,9 +119,10 @@ def detect(img_path):
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--image", required=True, help="path to the input image")
+    ap.add_argument("-d", "--debug", action='store_true', help="print some debug info")
     args = ap.parse_args()
-    
-    print(detect(args.image))
+
+    print(detect(args.image, args.debug))
 
 if __name__ == '__main__':
     main()
--- process.py
+++ process.py
@@ -3,30 +3,39 @@ import argparse
 import csv
 import cv2
 import pytesseract
-from PIL import Image
+from PIL import Image, ImageEnhance
 import numpy as np
 
 # from crop import crop
 
-def preprocess_for_ocr(img):
+def preprocess_for_ocr(img, enhance=1):
     """
     @param img: image to which the pre-processing steps being applied
     """
+    if enhance > 1:
+        img = Image.fromarray(img)
+
+        contrast = ImageEnhance.Contrast(img)
+
+        img = contrast.enhance(enhance)
+
+        img = np.asarray(img)
 
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    img = cv2.GaussianBlur(img, (5,5), 0)
+    #img = cv2.GaussianBlur(img, (5,5), 0)
 
     img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
 
     return img
 
-def ocr(img, oem=1):
+def ocr(img, oem=1, psm=3):
     """
     @param img: The image to be OCR'd
     @param oem: for specifying the type of Tesseract engine( default=1 for LSTM OCR Engine)
     """
-    config = ('-l eng --oem {} --psm 3'.format(oem))
+    config = ('-l eng --oem {oem} --psm {psm}'.format(oem=oem,psm=psm))
+    # config = ('-l eng --tessdata-dir "/usr/share/tesseract-ocr/tessdata" --oem {oem} -- psm {psm}'.format(oem=oem,psm=psm))
 
     try:
         img = Image.fromarray(img)
@@ -63,4 +72,4 @@ if __name__ == '__main__':
     #     text = ocr(cropped_image)
     #     print(text)
 
-    
+
